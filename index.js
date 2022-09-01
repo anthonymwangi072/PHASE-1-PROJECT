d407170a0f05e645673feccbd7dfaf57fa366bef
Anthony Mwaniki <anthonymwangi072@gmail.com>
2022-09-01 12:39:46 +0300

    created a function createMeal

--- index.js
+++ index.js
@@ -2,7 +2,26 @@ const getMealBtn = document.getElementById("get_meal");
 const mealContainer = document.getElementById('meal');
 // let likes;
 
+getMealBtn.addEventListener("click" , () => {
+    fetch("https://foodish-api.herokuapp.com/api/")
+     .then((res) => res.json())
+     .then((data) => {
+        console.log(data.image);
+        createMeal(data.image);
+      
 
+
+     })
+});
+function createMeal(meal) {
+    mealContainer.innerHTML = `
+    <div class = "row">
+    <div class = "column-five">
+    <img src= "${meal}" alt="meal Img" width="400px" height="400px"/>
+    </div>
+    </div>
+    `;
+}
 
 
 
